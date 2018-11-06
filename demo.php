73e650b49f9540289aa119551ca9d76d36d07d48
mengchenchen <1941077786@qq.com>
2018-11-06 12:41:32 +0800

    修改测试控制器

--- demo.php
+++ demo.php
@@ -5,8 +5,18 @@ include './vendor/autoload.php';
 $doc = new \Mengcc\Doc([
 	'path'      => './tests/Controllers/Api',
 	'api_name'  => 'mcc',
-	'namespace' => '\Mengcc\Tests\Controllers\Api\\'
+	'namespace' => '\Mengcc\Tests\Controllers\Api\\',
 ]);
 
-$doc->view('D://');
-echo(json_encode($doc->getAllData()));
+// 获取所有需要解析的参数
+$doc->getAllData();
+// echo(json_encode($doc->getAllData()));
+
+// 获取所有文件信息
+$doc->getFiles();
+// echo(json_encode($doc->getFiles()));
+
+// 生成静态 html文件，生成到 D 根目录下，命名为 mcc-doc
+$doc->view('D://', 'mcc-doc');
+
+
